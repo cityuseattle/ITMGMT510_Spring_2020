560e9b91966262260a066a3a67cd88471c8f2060
EuniceYuan <yhx710364284@gmail.com>
2020-04-09 15:53:17 -0700

    Submission for Module1

---
+++ IN/HuixianYuan/Module1/arithmetic.r
@@ -0,0 +1,21 @@
+#create variable A with value 1
+A<-1;
+#create variable B with value 2
+B<-2;
+#Calculate A divide B
+A/B;
+#Calculate A times B
+A*B;
+#Calculate A plus B
+A+B;
+#Calculate A subtract B
+A-B;
+#Calculate A to power of 2
+A^2;
+#Calculate B to power of 2
+B^2;
+
+# create C with text or character values
+C <- "hello world"
+# create D with value 1.2
+D <- 1.23
