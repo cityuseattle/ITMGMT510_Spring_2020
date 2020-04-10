279805767cc42c76ad6ed4a2de99ba396c589023
YUCHE-Liu <watch776@yahoo.com.rw>
2020-04-10 16:46:10 -0700

    Submission for Module 1

---
+++ IN/YuCheLiu/Module1/arithmetic.R
@@ -0,0 +1,17 @@
+a <-1;
+# create variable A with value -1
+b <-2;
+# create variable B with value -2
+
+a/b;
+#calculate A divide B
+a*b;
+#calculate A times B
+a+b;
+#calculate A plus B
+a-b;
+#calculate A subtract B
+a^2;
+#calculate A to power of 2
+b^2;
+#calculate B to power of 2
