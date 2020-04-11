9ca7297fd03fea8e1aeee7017da7ddd788c63d69
kimyou <kimyouby@hotmail.com>
2020-04-10 21:18:58 -0700

    Submission for Module1

---
+++ IN/KimyouBy/Module1/arithmetic.R
@@ -0,0 +1,18 @@
+#create variable a with value 1
+a <- 1;
+#create variable a with value 2
+b <- 2;
+
+#calculate a divides b
+a / b;
+#calculate a times b
+a * b;
+#calculate a plus b
+a + b;
+#calculate a subtracts b
+a - b;
+
+#calculate a to power of 2
+a ^ 2;
+#calculate b to power of 2
+b ^ 2;
