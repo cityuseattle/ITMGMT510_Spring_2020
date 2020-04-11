fa7a540f161040b84855d5dd42ef5316c40b8909
Triple_J <joker4jax@gmail.com>
2020-04-10 20:50:18 -0700

    Submission for Module 1

---
+++ IN/JiongjieJiang/Module1/arithmetic.R
@@ -0,0 +1,16 @@
+#create variable a with value 1
+a <- 1;
+#create variable b with value 2
+b <- 2;
+#calculate a divide b
+a / b;
+#calculate a times b
+a * b;
+#calculate a add b
+a + b;
+#calculate a minus b
+a - b;
+#calculate square of a 
+a^2;
+#calculate square of b
+b^2;
