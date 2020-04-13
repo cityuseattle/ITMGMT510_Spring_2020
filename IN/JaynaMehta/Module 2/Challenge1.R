2d0776fb2cd48f9811b5d8a44a81742f00d62f2d
jayna2203 <60206021+jayna2203@users.noreply.github.com>
2020-04-13 14:14:42 -0700

    Submission for Module 2

---
+++ IN/JaynaMehta/Module 2/Challenge1.R
@@ -0,0 +1,6 @@
+v <-c(length(rivers), sum(rivers) , mean(rivers), median(rivers), var(rivers), 
+sd(rivers), min(rivers), max(rivers))       # length of the rivers, sum of the rivers, mean of the rivers, median of the rivers
+                                            # variance of the rivers, standard deviation of the rivers, minimum number of the rivers
+                                            # maximum number of the rivers
+
+print(v)        # print the vector V
