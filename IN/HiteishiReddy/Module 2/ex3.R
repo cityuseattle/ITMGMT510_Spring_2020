e164629ad1ee3a91b3a4f5966e82ac8f99d1c453
tys13 <tysureddy@icloud.com>
2020-04-13 19:43:45 -0700

    Submission for Module2

---
+++ IN/HiteishiReddy/Module 2/ex3.R
@@ -0,0 +1,6 @@
+x <- c(300.99, 1.6, 583, 42.10)
+ 
+ print (floor(x));
+ print (ceiling(x));
+ print (trunc(x));
+ 
--- IN/HiteishiReddy/Module 2/printVector.R
+++ IN/HiteishiReddy/Module 2/printVector.R
@@ -1,2 +1,16 @@
 A <- c(1, 2, 3, 4, 5, 6);
 print (A);
+print(typeof(A));
+print(class(A));
+print(length(A));
+
+
+B <- 1:8
+print(B);
+print((B[2]));
+print(B[c(2,5)]);
+print(B[-2]);
+print(B[c(FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE)]);
+print(B[B>5])
+B[3] <- 9
+print(B)
