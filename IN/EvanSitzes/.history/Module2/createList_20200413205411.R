b265fd44d3272041e1ef3a5163e134384b0f6669
Evan <evan@simplymeasured.com>
2020-04-13 20:55:12 -0700

    Submission for Module 2

---
+++ IN/EvanSitzes/.history/Module2/createList_20200413205411.R
@@ -0,0 +1,12 @@
+A <- list("a". "b", 1, 2);
+print(A);
+print(str(A));
+print(typeof(A));
+print(length(A));
+print(A[1]);
+print(A[c(1:2)]);
+print(A[c(TRUE, FALSE, FALSE, FALSE)]);
+print(A[2] <- "n");
+print(A);
+print(A[2] <- NULL);
+print(A);
---
+++ IN/EvanSitzes/.history/Module2/printVector_20200413203300.R
@@ -0,0 +1,4 @@
+A <- c(1, 2, 3, 4, 5, 6);
+print(A);
+print(typeof(A));
+print(class(A));
