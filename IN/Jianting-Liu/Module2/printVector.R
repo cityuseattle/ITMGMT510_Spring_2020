39ab2c4be49b5ecb8b161df6b63aa7471dd0226b
GTian4MSM <jiantingmsm@gmail.com>
2020-04-13 21:08:31 -0700

    Submission for Module2

---
+++ IN/Jianting-Liu/Module2/printVector.R
@@ -0,0 +1,22 @@
+A <- c(1,8,3,4,5,6);
+print(A);
+print(typeof(A));
+print(class(A));
+print(length(A));
+
+# > print out the value greater than 5 not the index
+print(A[A > 5]); 
+
+B <- 1:8
+print(B);
+
+print((B[2]));
+print(B[c(2,5)]);
+print(B[-2]);
+
+print(B[c(FALSE, TRUE)]);
+
+print(B[B > 5]);
+
+B[3] <- 9
+print(B)
