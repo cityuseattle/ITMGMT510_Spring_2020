6b15f545b3a5cef73cd1eefa046aa8ec47409abe
Vincent-ZYS <1654209516@qq.com>
2020-04-14 00:30:41 -0700

    Submission of the Module2 HOP

---
+++ IN/YunshengZou/Module2/ex3.R
@@ -0,0 +1,8 @@
+x <- c(300.99,1.6,583,42.10);
+
+#floor(x) rounds to the largest integer not greater than x 
+#ceiling(x) rounds to the smallest integer not less than x 	
+#trunc(x) returns the integer part of x  
+print(floor(x));
+print(ceiling(x));
+print(trunc(x));
