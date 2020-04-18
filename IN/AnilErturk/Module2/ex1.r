5ea3616230ac904242bf704069a10c43faf78764
michael <erturkanil@cityuniversity.edu>
2020-04-18 00:13:03 -0700

    module2

---
+++ IN/AnilErturk/Module2/ex1.r
@@ -0,0 +1,5 @@
+# Create a vector v using "rivers" dataset.
+v <- c(length(rivers), sum(rivers), mean(rivers), median(rivers), var(rivers), 
+    sd(rivers), min(rivers), max(rivers))
+
+print(v) # Printf the vector v
