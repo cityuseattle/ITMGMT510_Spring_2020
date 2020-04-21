3dd189549b562086841a8ee616a1363073481285
Shanshan Yu <yushanshan@cityuniversity.edu>
2020-04-21 00:12:25 -0700

    Submission of Module3

---
+++ IN/ShanshanYu/Module3/createDataFrame.R
@@ -0,0 +1,12 @@
+A<-data.frame(emp_id=c(1,2,3),names=c("John","James","Mary"),salary=c(111.1,222.2,333.3));
+print(A);
+
+print(typeof(A));
+print(class(A));
+ncol(A);
+nrow(A);
+
+str(A);
+print(A["names"]);
+print(A$names);
+print(A[[2]]);
---
+++ IN/ShanshanYu/Module3/matrixManipulation.R
@@ -0,0 +1,21 @@
+A<-matrix(c(1,2,3,4,6,7,8,9,0),nrow=3,ncol=3,
+dimnames=list(c("X","Y","Z"),c("A","S","D")));
+# Shanshan Yu assginment
+print(A[-3,]);
+print(A[2,2]);
+print(A[A>4]);
+A[2,2]<-100;
+print(A);
+B<-rbind(A,c(1,2,3));
+print(B);
+C<-cbind(A,c(1,2,3));
+print(C);
+A<-t(A);
+print(A);
+#Challenge1
+x<-c(1,2,3)
+y<-c(4,5,6)
+z<-c(7,8,9)
+A<-cbind(x,y,z)
+rownames(A)<-c("a","b","c")
+print(A)
