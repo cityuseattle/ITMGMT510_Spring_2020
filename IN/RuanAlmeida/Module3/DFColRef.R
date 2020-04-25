6d4cb1070d0eb861e32e4f1efdd6c7990884f0bd
Ruan Almeida <ruanmurta@hotmail.com>
2020-04-24 21:54:50 -0700

    Submission for Module 3 part1 and 2

---
+++ IN/RuanAlmeida/Module3/DFColRef.R
@@ -0,0 +1,21 @@
+cars <- mtcars
+print(head(cars))
+
+colv1 <- cars$mpg
+print(colv1)
+
+colv2 <- cars[, 'mpg']
+print(colv2)
+
+colv3 <- cars[, 1]
+print(colv3)
+
+colv4 <- cars[['mpg']]
+print(colv4)
+
+mpgdf <- cars['mpg']
+print(head(mpgdf))
+
+mpgdf2 <- cars[1]
+print(head(mpgdf2))
+
