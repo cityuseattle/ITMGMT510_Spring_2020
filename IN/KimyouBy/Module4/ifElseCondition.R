5f3474f0ce1ba3fd074f81591f7bfb38f40d8a66
kimyou <kimyouby@hotmail.com>
2020-04-27 21:08:02 -0700

    Submission for Module4

---
+++ IN/KimyouBy/Module4/ifElseCondition.R
@@ -0,0 +1,12 @@
+#create vector
+A <- c(1,2); 
+#1 + 2
+B <- sum(A);
+
+if (B >= 5){
+    print("B is greater or equal to 5")
+}else if(B >= 3){
+    print("B is more than or equal to 3")
+}else{
+    print("B is less than 3")
+}
