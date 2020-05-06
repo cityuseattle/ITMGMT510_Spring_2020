ec0d8264baa7a713e1f64b492db3651af10e48cc
DuttaSmita <duttasmita@cityuniversity.edu>
2020-05-05 17:01:19 -0700

    Submission for Module5

--- IN/SmitaDutta/Module4/breakKey.R
+++ IN/SmitaDutta/Module4/breakKey.R
@@ -8,3 +8,6 @@ break;
 print(e);
 
 }
+
+
+
---
+++ IN/SmitaDutta/Module5/sortData.R
@@ -0,0 +1,4 @@
+data <- read.csv(file = "iris2.csv", header =TRUE, sep = ",")
+ #print (data)
+
+ print(data[order(data$petal.width, decreasing =TRUE), ])
