86d1600c684da6ce5edeefc1a66c178dd7bab797
hunarN <hunar.nadha@gmail.com>
2020-05-07 21:39:22 -0700

    Submission for module 5

---
+++ IN/HunarNadha/Module5/selectData.r
@@ -0,0 +1,7 @@
+#Print selected data for variety "Setosa"
+data <- read.csv(file = "iris2.csv", header=TRUE, sep=",")
+res <- subset(data, variety=="Setosa")
+#print(res)
+
+#Write filtered data into a new CSV file
+write.csv(res,"result.csv")
---
+++ IN/HunarNadha/Module5/sortData.r
@@ -0,0 +1,4 @@
+#Sort the data ascending in petal.width column
+data <- read.csv(file = "iris2.csv", header=TRUE, sep=",")
+
+print(data[ order(data$petal.width, decreasing=TRUE), ])
