fa65f6953d9874791c8fa5bdf98c519a08d8dca6
leobaiiii <leobai9188@gmail.com>
2020-05-13 04:09:49 -0700

    Module6

---
+++ IN/YitaoBai/Module6/dplrPkg.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(nycflights13)
+
+print(head(filter(flights,month==11,day==3,carrier=='AA')))
+
+print(slice(flights,1:10))
+
+print(head(arrange(flights,year,month,day,sched_arr_time)))
+
+print(head(select(flights,carrier)))
+
+print(head(rename(flights,airline_month = month)))
+
+print(distinct(select(flights,carrier)))
+
+print(summarise(flights,avg_air_time=mean(air_time,na.rm=TRUE)))
+
+print(sample_n(flights,10))
