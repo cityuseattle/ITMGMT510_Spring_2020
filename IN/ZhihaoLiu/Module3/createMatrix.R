8b921505bde67e6195a59495160ceb918c94a83b
0 03 <you@example.comqwyp003@gmail.com>
2020-05-17 21:03:09 -0700

    Sub for missing modules

---
+++ IN/ZhihaoLiu/Module3/createMatrix.R
@@ -0,0 +1,19 @@
+A <- matrix(c(1,2,3,4,6,7,8,9,0),nrow=3,ncol=3,
+dimnames=list(c("X","Y","Z"),c("A","S","D")));
+
+print(A);
+print(class(A));
+print(attributes(A));
+
+print(colnames(A))
+print(rownames(A))
+
+B <- cbind(c(1,2,3),c(4,5,6));
+print(B)
+
+C <- rbind(c(1,2,3),c(4,5,6));
+print(C);
+
+print(A[1,]);
+
+print(A[,1]);
