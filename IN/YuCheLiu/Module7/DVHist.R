423624295b470c6021b0ff5ab4b2fe9f4257ce6b
YUCHE-Liu <watch776@yahoo.com.rw>
2020-05-18 21:20:49 -0700

    Submission for Module7

---
+++ IN/YuCheLiu/Module7/DVHist.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+library(ggplot2movies)
+df <- movies <- movies[sample(nrow(movies),1000),]
+
+print(head(df))
+
+pl <- ggplot(df, aes(x=rating))
+
+print(pl + geom_histogram(binwidth=0.1,
+                            color='red',
+                            fill='pink', 
+                            alpha=0.4,
+                            linetype='dotted')
+        + xlab('Movie Ratings')
+        +ylab('Occurences')
+        +ggtitle('Movie Rating'))
