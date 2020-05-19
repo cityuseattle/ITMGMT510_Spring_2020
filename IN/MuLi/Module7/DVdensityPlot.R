dde006603aff3f9dbbf8ce78def38b355e87400c
Frodice <56413776+Frodice@users.noreply.github.com>
2020-05-20 04:42:16 +0800

    Submission for Module7

---
+++ IN/MuLi/Module7/DVdensityPlot.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+library(ggplot2movies)
+
+df <- movies<-movies[sample(nrow(movies), 1000), ]
+
+# print(head(df))
+
+pl <- ggplot(df,aes(x=rating))
+
+print(pl + geom_histogram(binwidth=0.1,aes(y=..density..)) + geom_density(color='red'))
