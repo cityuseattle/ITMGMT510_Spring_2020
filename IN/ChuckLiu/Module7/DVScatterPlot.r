90406e8ebf26bf6d8685c289bbfd0ee21c99be6f
chuckbbz <chucklpf@gmail.com>
2020-05-18 20:58:10 -0700

    Module 7

---
+++ IN/ChuckLiu/Module7/DVScatterPlot.r
@@ -0,0 +1,12 @@
+library(ggplot2);
+
+df <- mtcars;
+
+print(head(df));
+
+#Data & Aesthetics layer
+pl <- ggplot(data = df, aes (x = wt, y = mpg));
+
+#Geometry layer
+#Adding the data & Aesthetics layer to geom layer
+print(pl + geom_point(aes(color = cyl)));
