f0337bfba106f3886c9d0dcd7fb1f47208b375a1
dmdm54 <2575530785@qq.com>
2020-05-18 23:17:28 -0700

    Submission for Module 7

---
+++ IN/ZhihaoZheng/Module7/DVHistAdunc.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+library(ggplot2movies)
+
+df <- movies<-movies[sample(nrow(movies), 1000),]
+
+pl<- ggplot(df,aes(x=rating))
+
+pl2<- pl + geom_histogram(binwidth=0.1, aes(fill=..count..)) + xlab('Movie Rating') + ylab('Occurences')
+
+print(pl2 + scale_fill_gradient('Count',low='blue', high='red'))
