c09cd62b1b28378f808dea0aaccd0d5e053b7293
Triple_J <joker4jax@gmail.com>
2020-05-21 15:33:30 -0700

    Submission for Module 7

---
+++ IN/JiongjieJiang/Module7/DVHist.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(ggplot2movies)
+df <- movies <- movies[sample(nrow(movies), 1000), ]
+
+#print(head(df))
+
+#DATA & AESTHETICS Layer
+#Build a histogram out of rating colum
+pl <- ggplot(df, aes(x = rating))
+
+#Geometry layer
+#Adding the data & AESTHETICS layer with geom layer
+print(pl + geom_histogram(binwidth = 0.1, color = 'red', fill = 'pink', alpha = 0.4, linetype = 'dotted')
+         + xlab('Movie Ratings') + ylab('Occurences') + ggtitle('Movie Ratings'))
