3f60fe3810afc477236f71f7e35f12d2e584baff
Mat-Ke <phiqword@gmail.com>
2020-05-23 13:14:26 +0800

    Submission for Module 7

---
+++ IN/MatthewKe/Module7/DV-barPlot.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+
+df <- mpg
+print(head(df))
+#DATA & AESTHETICS Layer
+#counts (or sums of weights)
+#The x axis will be the Class col
+#and the y axis will be the counts of
+#the occurance of each elements in the class col
+g <- ggplot(df, aes(class))
+
+#Geometry layer
+print(g + geom_bar(aes(fill = drv))) 
---
+++ IN/MatthewKe/Module7/DV-boxPlot.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+
+df <- mtcars
+
+print(head(df))
+
+#DATA & AESTHETICS Layer
+
+pl <- ggplot(data = df, aes(factor(cyl), mpg))
+
+#Geometry layer
+#Adding the data & AESTHETICS layer with geom layer
+print(pl + geom_boxplot() + coord_flip()) 
