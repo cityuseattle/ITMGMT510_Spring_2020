fa9e77e0bf886be91f1e33221cff8cda1f38bbd1
zhui <392513460@qq.com>
2020-05-29 14:17:40 -0700

    Submission module8

---
+++ IN/Yi Zhou/Module 8/LineReg.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(ggthemes)
+library(dplyr)
+library(corrplot)
+library(corrgram)
+
+df <- read.csv ('student-mat.csv' , header=TRUE, sep=';')
+
+print(head(df))
+
+
+print(any(is.na(df)))
+
+num.cols <- sapply(df, is.numeric)
+
+cor.data <- cor(df[,num.cols])
+
+corrplot(cor.data,method='color')
+
+print(ggplot(df,aes(x=G3))+ geom_histogram(bins=20,alpha=0.5, fill='blue') +theme_minimal())
---
+++ IN/Yi Zhou/Module 8/LineReg2.R
@@ -0,0 +1,58 @@
+library(caTools)
+library(ggplot2)
+
+df <- read.csv('student-mat.csv',header=TRUE, sep=';')
+
+set.seed(101)
+
+sample <- sample.split(df$G3, SplitRatio = 0.70)
+
+train = subset(df, sample == TRUE)
+test = subset(df, sample == FALSE)
+
+model <- lm(G3 ~.,train)
+
+print(summary(model))
+
+res <- residuals(model)
+
+res <- as.data.frame(res)
+
+print(head(res))
+
+G3.predictions <- predict(model, test)
+
+results <- cbind(G3.predictions,test$G3)
+
+colnames(results) <- c('pred','real')
+
+results <- as.data.frame(results)
+print(head(results))
+
+to_zero <- function(x)
+{
+    if (x < 0)
+    {
+        return(0)
+    }
+    else 
+    {
+        return(x)
+    }  
+}
+
+results$pred <- sapply(results$pred,to_zero)
+
+print(min(results))
+
+mse <- mean((results$real-results$pred)^2)
+
+print ('MSE')
+print (mse)
+
+SSE = sum((results$pred - results$real)^2)
+SST = sum( (mean(df$G3) - results$real)^2)
+
+R2 = 1 - SSE/SST
+
+print(R2)
