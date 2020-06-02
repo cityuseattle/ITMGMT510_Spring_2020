86fff3127530e53743bb0ed76b17aec2d1d8fffc
DuttaSmita <duttasmita@cityuniversity.edu>
2020-06-01 19:42:47 -0700

    Submission for Module 9

---
+++ IN/SmitaDutta/Module9/kNN.R
@@ -0,0 +1,64 @@
+library(ISLR)
+
+print(str(Caravan))
+
+print(summary(Caravan$Purchase))
+
+print(any(is.na(Caravan)))
+
+purchase <- Caravan[,86]
+
+standardized.Caravan <- scale(Caravan[, -86])
+
+print (var(Caravan[,1]))
+
+print (var(Caravan[,2]))
+
+test.index <- 1:1000
+test.data <- standardized.Caravan[test.index,]
+test.purchase <- purchase[test.index]
+
+
+train.data <- standardized.Caravan[-test.index,]
+train.purchase <- purchase[-test.index]
+
+library(class)
+
+
+set.seed(101)
+
+predicted.purchase <- knn(train.data, test.data, train.purchase, k=5)
+print(head(predicted.purchase))
+misclarification.error <- mean(test.purchase != predicted.purchase)
+
+print(misclarification.error)
+
+
+predicted.purchase= NULL
+error.rate =NULL
+
+for(i in 1:20){
+    set.seed(101)
+    predicted.purchase <- knn(train.data, test.data, train.purchase, k=i)
+    error.rate[i] =mean(test.purchase != predicted.purchase)
+}
+
+#print(error.rate)
+
+library(ggplot2)
+k.values <- 1:20
+error.df <- data.frame(error.rate, k.values)
+print(error.df)
+
+print(ggplot(error.df, aes(x=k.values, y=error.rate)) + geom_point() + geom_line(lty = "dotted", color ='red'))
+
+
+
+
+
+
+
+
+
+
+
