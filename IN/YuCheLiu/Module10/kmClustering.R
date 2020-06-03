1fd27a6d8fb19cf12dc2e877b0d8465570435430
YUCHE-Liu <watch776@yahoo.com.rw>
2020-06-02 18:48:06 -0700

    Submission for Module 10

---
+++ IN/YuCheLiu/Module10/kmClustering.R
@@ -0,0 +1,18 @@
+library(ISLR)
+library(ggplot2)
+
+print(head(iris))
+
+pl <- ggplot(iris, aes(Petal.Length, Petal.Width, color = Species)) + geom_point()
+
+
+set.seed(101)
+
+irisCluster <- kmeans((iris[,1:4]), 3,  nstart = 10)
+
+pl <- table(irisCluster$cluster, iris$Species)
+print(pl)
+
+library(cluster)
+
+clusplot(iris, irisCluster$cluster, color=TRUE, shade=TRUE, labels=0, lines=0)
