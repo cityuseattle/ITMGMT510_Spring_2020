9408e0bffb9b446b320f56ebdb7c954cdfcbccb1
Satorizer <37605323+Satorizer@users.noreply.github.com>
2020-06-05 12:12:31 -0700

    Submission for Module 9

---
+++ IN/DingmingMo/Module9/kNN.R
@@ -0,0 +1,43 @@
+library(ISLR)
+
+print(str(Caravan))
+
+print(summary(Caravan$Purchase))
+
+print(any(is.na(Caravan)))
+
+purchase <- Caravan[,86]
+
+standarized.Caravan <- scale(Caravan[,-86])
+
+print(var(standarized.Caravan[,1]))
+
+print(var(standarized.Caravan[,2]))
+
+test.index <- 1:1000
+test.data <- standarized.Caravan[test.index,]
+test.purchase <- purchase[test.index]
+
+train.data <- standarized.Caravan[-test.index,]
+train.purchase <- purchase[-test.index]
+
+library(class)
+
+predicted.purchase = NULL
+error.rate = NULL
+
+for(i in 1:20)
+{
+    set.seed(101)
+    predicted.purchase = knn(train.data, test.data, train.purchase, k=i)
+    error.rate[i] = mean(test.purchase != predicted.purchase)
+}
+
+
+library(ggplot2)
+k.values <- 1:20
+error.df <- data.frame(error.rate, k.values)
+
+print(error.df)
+
+print(ggplot(error.df, aes(x = k.values, y = error.rate)) + geom_point() + geom_line(lty = "dotted", color = 'red'))
