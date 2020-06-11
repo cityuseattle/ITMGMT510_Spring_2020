4f895f684b6c71342a04ff94c2586884e8ea7c07
Ruan Almeida <ruanmurta@hotmail.com>
2020-06-11 00:51:58 -0700

    Submission for Module10

---
+++ IN/RuanAlmeida/Module10/kmClustering.R
@@ -0,0 +1,14 @@
+library(ISLR)
+library(ggplot2)
+
+print(head(iris))
+pl <- ggplot(iris, aes(Petal.Length, Petal.Width, color =Species)) + geom_point()
+set.seed(101)
+irisCluster <- kmeans(iris[,1:4], 3, nstart = 20)
+print(irisCluster)
+
+pl <- table(irisCluster$Cluster, iris$Species)
+print(pl)
+
+library(cluster)
+clusplot(iris, irisCluster$cluster, color=TRUE, shade=TRUE, labels=0,lines=0, )
